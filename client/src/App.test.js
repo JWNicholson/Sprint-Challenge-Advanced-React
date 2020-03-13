c41d5a08c57219a6b827846a38808c1d871d3b1b
JW Nicholson <john.nicholson812@gmail.com>
2020-03-13 13:33:08 -0400

    met MVP. Spectrums is down. will push as soon as able

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import Navbar from "./components/Navbar";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("App renders without crashing", () => {
+   //console.log(component);
+  const component = render(<App />);
 });
+
+test("Navbar renders successfully", () => {
+  const component = render(<Navbar />);
+})
+
+test("Header content rendered successfully", () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/search popularity/i))
+})
+
+test("darkMode toggle switch renders successfully", () => {
+  const { findAllByDisplayValue } = render(<App />);
+  findAllByDisplayValue(/toggle/i)
+})
+
+test(".playersDiv renders", () => {
+  const { findAllByDisplayValue } = render(<App />);
+  findAllByDisplayValue(/playersDiv/i)
+})
